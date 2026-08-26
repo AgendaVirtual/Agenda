fdb6f7a816c7cb638772dc25ad1ecd6f7754b8c6
hazdriel <62810790+hazdriel@users.noreply.github.com>
2026-08-26 10:33:36 -0300

    Corrige resolver IPv6 do nginx no Railway

--- frontend/docker-entrypoint.d-10-resolver.sh
+++ frontend/docker-entrypoint.d-10-resolver.sh
@@ -1,12 +1,23 @@
 #!/bin/sh
 set -eu
 
-servidores=$(awk '/^nameserver/ { printf "%s ", $2 }' /etc/resolv.conf)
+servidores=$(
+  awk '
+    /^nameserver/ {
+      servidor = $2
+      if (index(servidor, ":") > 0) {
+        servidor = "[" servidor "]"
+      }
+      printf "%s%s", separador, servidor
+      separador = " "
+    }
+  ' /etc/resolv.conf
+)
 
 if [ -z "$servidores" ]; then
   echo "10-resolver: sem nameserver em /etc/resolv.conf, seguindo sem resolver" >&2
   exit 0
 fi
 
-echo "resolver ${servidores}valid=30s;" > /etc/nginx/conf.d/resolver.conf
+echo "resolver ${servidores} valid=30s;" > /etc/nginx/conf.d/resolver.conf
 echo "10-resolver: usando ${servidores}"
